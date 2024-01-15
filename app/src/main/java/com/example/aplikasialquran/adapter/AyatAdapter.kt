2605971880229077560a8c2f837e88311c2b0467
mzuhairhamzah <zuhair.hamzah08@gmail.com>
2024-01-15 09:42:40 +0800

    add package, import

---
+++ app/src/main/java/com/example/aplikasialquran/adapter/AyatAdapter.kt
@@ -0,0 +1,9 @@
+package com.example.aplikasialquran.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.aplikasialquran.R
+import com.example.aplikasialquran.model.ModelAyat
