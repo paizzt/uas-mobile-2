89555182bf10a719d171e6792909f3df7dc346fb
mzuhairhamzah <zuhair.hamzah08@gmail.com>
2024-01-15 10:03:05 +0800

    add fun loadjadwal api jadwal sholat  dan kota

--- app/src/main/java/com/example/aplikasialquran/fragment/FragmentJadwalSholat.kt
+++ app/src/main/java/com/example/aplikasialquran/fragment/FragmentJadwalSholat.kt
@@ -86,3 +86,38 @@ class FragmentJadwalSholat : BottomSheetDialogFragment() {
 
         return v
     }
+
+       @SuppressLint("SimpleDateFormat")
+    private fun loadJadwal(id: Int?) {
+        try {
+            progressDialog!!.show()
+            val idKota = id.toString()
+            val current = SimpleDateFormat("yyyy-MM-dd")
+            val tanggal = current.format(Date())
+            val url = "https://api.banghasan.com/sholat/format/json/jadwal/kota/$idKota/tanggal/$tanggal"
+            val task = ClientAsyncTask(this, object : ClientAsyncTask.OnPostExecuteListener {
+                override fun onPostExecute(result: String) {
+                    try {
+                        progressDialog!!.dismiss()
+                        val jsonObj = JSONObject(result)
+                        val objJadwal = jsonObj.getJSONObject("jadwal")
+                        val obData = objJadwal.getJSONObject("data")
+
+                        tv_subuh.text = obData.getString("subuh")
+                        tv_dzuhur.text = obData.getString("dzuhur")
+                        tv_ashar.text = obData.getString("ashar")
+                        tv_maghrib.text = obData.getString("maghrib")
+                        tv_isya.text = obData.getString("isya")
+
+                    } catch (e: JSONException) {
+                        e.printStackTrace()
+                    }
+                }
+
+            })
+            task.execute(url)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+    }
